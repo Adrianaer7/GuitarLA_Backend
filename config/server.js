5c71fd5c8168ed41225a5099cce066eb9453bd56
Adrian Roldán <adrianaer7@hotmail.com>
2022-05-03 15:58:08 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b4be20388cd1a66a904f53b163b1579a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a88ea7d5-eab6-4190-8fe5-56573be3f7ce'
+};
